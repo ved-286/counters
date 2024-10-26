108833b0039e87ed9dcc5344a44846ec4cc6f7d9
ved-286 <patelved1921@gmail.com>
2024-10-26 02:39:02 -0700

    added ui with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,24 @@ if (count >= 20){
 }
 
   return (
-    <>
-    <h1>count your number</h1>
-    <h2>{count}</h2>
-
-    <button onClick={Add}> Add value :{count}</button>
-    <button onClick={remove}> remove value :{count}</button>
-    </>
+   
+    <div className='   h-full flex flex-row min-h-screen justify-center items-center bg-slate-900 ' >
+        <div className='bg-slate-800 w-150 h-100 text-center rounded-md shadow-md p-10'>
+           <h1 className='font-sans font-serif uppercase font-semibold text-white mb-4'>
+            Count your number
+            </h1>
+              <div className='bg-sky-400 w-7 h-7  rounded-full w-1/2 mx-auto mb-5'>
+                 <h2 >{count}</h2>
+              </div>
+                   <button onClick={Add} className='rounded-md bg-green-700 text-white p-4 m-4'> Add value : {count}</button>
+                  
+                   <button onClick={remove} className='rounded-md bg-red-400 text-white p-4 m-4'> Remove value : {count}</button>
+        </div>
+                  <div>
+                   {count >= 20 && <p>Limit reached!</p>}
+                   </div>
+    </div>
+   
     
   )
 }
